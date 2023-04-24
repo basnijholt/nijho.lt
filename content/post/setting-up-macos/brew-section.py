d269452f8fef0773d1bf26479d07c6964c985620
Bas Nijholt <bas@nijho.lt>
2023-04-23 17:04:22 -0700

    Add formula

--- content/post/setting-up-macos/brew-section.py
+++ content/post/setting-up-macos/brew-section.py
@@ -2,30 +2,42 @@ import json
 
 
 def generate_markdown(json_data):
-    markdown = ""
-    for i, (category, apps) in enumerate(json_data.items(), start=1):
-        markdown += f"### {i}. {category}\n\n"
-        for app in apps:
-            markdown += f"#### {app['name']}\n"
-            markdown += f"{app['description']}\n"
-            maybe_cask = " --cask" if app["cask"] else ""
-            markdown += f"* Install with `brew install{maybe_cask} {app['brew']}`\n"
-            repo = app.get("repo")
-            docs = app.get("docs")
-            if repo:
-                fa = '<em class="fab fa-github fa-fw" style="margin-right: 0.2em;"> </em>'
-                repo = f" [repo {fa}]({repo})"
-            if docs:
-                fa = '<em class="fas fa-book fa-fw" style="margin-right: 0.2em;"> </em>'
-                docs = f" [docs {fa}]({docs})"
-            if docs and repo:
-                markdown += f"* See the {docs} and {repo}\n"
-            elif docs:
-                markdown += f"* See the {docs}\n"
-            elif repo:
-                markdown += f"* See the {repo}\n"
-            markdown += "\n"
-    return markdown
+    def generate_app_section(app):
+        maybe_cask = " --cask" if app["cask"] else ""
+        base = (
+            "https://formulae.brew.sh/cask/"
+            if app["cask"]
+            else "https://formulae.brew.sh/formula/"
+        )
+        lines = []
+        brew_formula = base + app["brew"]
+        if "/" not in app["brew"]:
+            formula_line = f'[formula <em class="fas fa-flask fa-fw" style="margin-right: 0.2em;"> </em>]({brew_formula})'
+            lines.append(formula_line)
+        if repo := app.get("repo"):
+            repo_line = f'[repo <em class="fab fa-github fa-fw" style="margin-right: 0.2em;"> </em>]({repo})'
+            lines.append(repo_line)
+        if docs := app.get("docs"):
+            docs_line = f'[docs <em class="fas fa-book fa-fw" style="margin-right: 0.2em;"> </em>]({docs})'
+            lines.append(docs_line)
+        if lines:
+            if len(lines) == 2:
+                lines = [f"{lines[0]} and {lines[1]}"]
+            elif len(lines) > 2:
+                lines[-1] = f"and {lines[-1]}"
+            references = f"* See the {', '.join(lines)}\n"
+
+        return (
+            f"#### {app['name']}\n"
+            f"{app['description']}\n"
+            f"* Install with `brew install{maybe_cask} {app['brew']}`\n"
+            f"{references}\n"
+        )
+
+    return "\n".join(
+        f"### {i}. {category}\n\n" + "".join(generate_app_section(app) for app in apps)
+        for i, (category, apps) in enumerate(json_data.items(), start=1)
+    )
 
 
 with open("brew.json", "r") as json_file:
